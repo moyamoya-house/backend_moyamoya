ae72596af246126f6e29c3df116dd5df640c873f
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-06-11 22:14:14 +0900

    init作成

--- project/__init__.py
+++ project/__init__.py
@@ -7,6 +7,11 @@ db = SQLAlchemy()
 
 migrate = Migrate()
 
-def create_app():
+def create_app(config_filename ="config.py"):
     app= Flask(__name__)
+    app.config.from_pyfile(config_filename)
+    app.config['DEBUG'] = True
+    db.init_app(app)
+    migrate.init_app(app)
     
+    return app

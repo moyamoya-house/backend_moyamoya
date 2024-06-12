9ec16ebbeb5973a4c6df3267c01914c058db895e
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-06-12 22:03:34 +0900

    flask-cors compose

--- project/__init__.py
+++ project/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_cors import CORS
 import os
 
 db = SQLAlchemy()
@@ -10,6 +11,8 @@ migrate = Migrate()
 
 login_manager = LoginManager()
 
+cors = CORS()
+
 def create_app(config_filename ="config.py"):
     app= Flask(__name__)
     app.config.from_pyfile(config_filename)
@@ -17,6 +20,7 @@ def create_app(config_filename ="config.py"):
     db.init_app(app)
     migrate.init_app(app,db)
     login_manager.init_app(app)
+    cors.init_app(app)
     
     from project.models import User
     @login_manager.user_loader
